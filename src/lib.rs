2e88e34dfecaff8b9be63cc5c84a161253ac19fc
BurntPizza <xburntpizza@gmail.com>
2017-01-30 20:40:54 -0500

    Cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,11 @@
 
-// TODO: NOT READY FOR PUBLISHING AT ALL
+use std::fmt::Debug;
 
-
-
-use std::fmt::{self, Debug, Formatter};
-
-#[cfg(target_pointer_width = "64")]
-const K: u8 = 64;
-
-#[cfg(target_pointer_width = "32")]
-const K: u8 = 32;
+pub mod methods;
 
 pub trait HashFn: Debug {
     fn hash(&self, input: usize) -> usize;
-    fn size(&self) -> usize;
+    fn max(&self) -> usize;
 }
 
 pub trait HashMethod {
@@ -68,12 +60,12 @@ impl<T: Clone> Tree<T> {
             return Some(Tree::Leaf(Marker::Case(cases[0], data[0].clone())));
         }
 
-        let mut best: Option<Tree<T>> = None;
+        let mut best = None;
 
         for m in methods {
             let f = m.new(cases);
-            let size = f.size();
-            let children: Vec<_> = (0..size)
+            let max = f.max();
+            let children: Vec<_> = (0..max)
                 .into_iter()
                 .map(|slot| {
                     let (cases, data): (Vec<_>, Vec<_>) = cases.iter()
@@ -118,132 +110,7 @@ impl<T: Clone> Tree<T> {
     }
 }
 
-pub struct ClzSub;
 
-struct ClzSubFn {
-    bias: usize,
-    size: usize,
-}
-
-impl HashFn for ClzSubFn {
-    fn hash(&self, v: usize) -> usize {
-        (v.leading_zeros() as usize) - self.bias
-    }
-
-    fn size(&self) -> usize {
-        self.size
-    }
-}
-
-impl Debug for ClzSubFn {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "clz(x) - {}", self.bias)
-    }
-}
-
-impl HashMethod for ClzSub {
-    fn new(&self, case_set: &[usize]) -> Box<HashFn> {
-        assert!(!case_set.is_empty());
-        let bias =
-            case_set.iter().cloned().map(|v| ClzSubFn { bias: 0, size: 0 }.hash(v)).min().unwrap();
-        let max_size = case_set.into_iter()
-            .map(|&v| {
-                ClzSubFn {
-                        bias: bias,
-                        size: 0,
-                    }
-                    .hash(v)
-            })
-            .max()
-            .unwrap() + 1;
-        Box::new(ClzSubFn {
-            bias: bias,
-            size: max_size,
-        })
-    }
-}
-
-/// From "Efficient Multiway Radix Search Trees"
-/// https://drhanson.s3.amazonaws.com/storage/documents/mrst.pdf
-pub struct ShiftMask;
-
-impl HashMethod for ShiftMask {
-    fn new(&self, case_set: &[usize]) -> Box<HashFn> {
-        Box::new(Window::critical_window(case_set))
-    }
-}
-
-#[derive(Copy, Clone)]
-pub struct Window {
-    pub l: u8,
-    pub r: u8,
-}
-
-impl Window {
-    pub fn is_critical(&self, cases: &[usize]) -> bool {
-        let thresh = 1 << (self.l - self.r);
-        let cardinality = mapped_cardinality(cases, |v| self.hash(v));
-
-        cardinality > thresh
-    }
-
-    /// Find the longest critical window, and the most critical window of that length.
-    pub fn critical_window(cases: &[usize]) -> Window {
-        let mut w = Window {
-            l: K - 1,
-            r: K - 1,
-        };
-        let mut w_max = w;
-        for b in (0..K).rev() {
-            w.r = b;
-            if w.is_critical(cases) {
-                w_max = w;
-            } else {
-                w.l -= 1;
-                if mapped_cardinality(cases, |v| w.hash(v)) >
-                   mapped_cardinality(cases, |v| w_max.hash(v)) {
-                    w_max = w;
-                }
-            }
-        }
-
-        w_max
-    }
-}
-
-impl HashFn for Window {
-    fn hash(&self, input: usize) -> usize {
-        let width = 1 + self.l - self.r;
-        let mask = (1 << width) - 1;
-        (input >> self.r) & mask
-    }
-
-    fn size(&self) -> usize {
-        1 << (1 + (self.l - self.r) as usize)
-    }
-}
-
-impl Debug for Window {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        // format!("shr {}\nand {}", w.r, 1 + w.l - w.r)
-        if self.l == self.r {
-            write!(f, "bit {}", self.l)
-        } else {
-            write!(f, "bits {} to {}", self.l, self.r)
-        }
-    }
-}
-
-// The 'score' of hash function `f` on dataset `c`.
-// The score is higher the fewer collisions `f` produces on `c`.
-fn mapped_cardinality<F>(cases: &[usize], f: F) -> usize
-    where F: Fn(usize) -> usize
-{
-    let mut set: Vec<_> = cases.into_iter().map(|&s| f(s)).collect();
-    set.sort();
-    set.dedup();
-    set.len()
-}
 
 // TODO: look for hash functions with few collisions
 // use additional node types for this: e.g. "Node" -> "ShiftMask" or something
@@ -258,13 +125,15 @@ mod tests {
     use std::fs::File;
     use std::fmt::Display;
 
-    use super::{HashFn, ShiftMask, ClzSub, Window, Tree, Marker};
+    use super::{HashFn, Tree, Marker};
+    use super::methods::*;
 
     #[test]
     fn test_val() {
         assert_eq!(Window { l: 5, r: 3 }.hash(41), 5);
     }
 
+    #[ignore]
     #[test]
     fn it_works() {
         let set = vec![// (0, "F"),
---
+++ src/methods.rs
@@ -0,0 +1,135 @@
+
+use std::fmt::{self, Debug, Formatter};
+
+use {HashMethod, HashFn};
+
+
+#[cfg(target_pointer_width = "64")]
+const K: u8 = 64;
+
+#[cfg(target_pointer_width = "32")]
+const K: u8 = 32;
+
+
+
+pub struct ClzSub;
+
+struct ClzSubFn {
+    bias: usize,
+    max: usize,
+}
+
+impl HashFn for ClzSubFn {
+    fn hash(&self, v: usize) -> usize {
+        (v.leading_zeros() as usize) - self.bias
+    }
+
+    fn max(&self) -> usize {
+        self.max
+    }
+}
+
+impl Debug for ClzSubFn {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "clz(x) - {}", self.bias)
+    }
+}
+
+impl HashMethod for ClzSub {
+    fn new(&self, case_set: &[usize]) -> Box<HashFn> {
+        assert!(!case_set.is_empty());
+        let bias =
+            case_set.iter().cloned().map(|v| ClzSubFn { bias: 0, max: 0 }.hash(v)).min().unwrap();
+        let max = case_set.into_iter()
+            .map(|&v| {
+                ClzSubFn {
+                        bias: bias,
+                        max: 0,
+                    }
+                    .hash(v)
+            })
+            .max()
+            .unwrap() + 1;
+        Box::new(ClzSubFn {
+            bias: bias,
+            max: max,
+        })
+    }
+}
+
+/// From "Efficient Multiway Radix Search Trees"
+/// https://drhanson.s3.amazonaws.com/storage/documents/mrst.pdf
+pub struct ShiftMask;
+
+impl HashMethod for ShiftMask {
+    fn new(&self, case_set: &[usize]) -> Box<HashFn> {
+        Box::new(Window::critical_window(case_set))
+    }
+}
+
+#[derive(Copy, Clone)]
+pub struct Window {
+    pub l: u8,
+    pub r: u8,
+}
+
+impl Window {
+    pub fn is_critical(&self, cases: &[usize]) -> bool {
+        let thresh = 1 << (self.l - self.r);
+        let cardinality = self.mapped_cardinality(cases);
+
+        cardinality > thresh
+    }
+
+    /// Find the longest critical window, and the most critical window of that length.
+    pub fn critical_window(cases: &[usize]) -> Window {
+        let mut w = Window {
+            l: K - 1,
+            r: K - 1,
+        };
+        let mut w_max = w;
+        for b in (0..K).rev() {
+            w.r = b;
+            if w.is_critical(cases) {
+                w_max = w;
+            } else {
+                w.l -= 1;
+                if w.mapped_cardinality(cases) > w_max.mapped_cardinality(cases) {
+                    w_max = w;
+                }
+            }
+        }
+
+        w_max
+    }
+
+    fn mapped_cardinality(&self, cases: &[usize]) -> usize {
+        let mut set: Vec<_> = cases.into_iter().map(|&s| self.hash(s)).collect();
+        set.sort();
+        set.dedup();
+        set.len()
+    }
+}
+
+impl HashFn for Window {
+    fn hash(&self, input: usize) -> usize {
+        let width = 1 + self.l - self.r;
+        let mask = (1 << width) - 1;
+        (input >> self.r) & mask
+    }
+
+    fn max(&self) -> usize {
+        1 << (1 + (self.l - self.r) as usize)
+    }
+}
+
+impl Debug for Window {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        // format!("shr {}\nand {}", w.r, 1 + w.l - w.r)
+        if self.l == self.r {
+            write!(f, "bit {}", self.l)
+        } else {
+            write!(f, "bits {} to {}", self.l, self.r)
+        }
+    }
+}
